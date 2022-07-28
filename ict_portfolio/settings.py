16d38a174f5d569003fa6a26d1baa9a272103ffc
isaacmaredi <isaacmaredi@icloud.com>
2022-07-28 16:06:00 +0200

    added local_settings import

--- ict_portfolio/settings.py
+++ ict_portfolio/settings.py
@@ -150,3 +150,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
